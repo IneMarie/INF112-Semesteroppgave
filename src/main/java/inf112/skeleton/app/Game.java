8eaf7282e58c9a459cf226abece60f2c1bb849aa
Ivar Fatland <ivar.fatland@lyse.net>
2024-02-23 11:40:25 +0100

    Changed Game to show a CameraController2D example

--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.Texture;
 
@@ -16,9 +16,7 @@ public class Game implements ApplicationListener {
 	private BitmapFont font;
 	private Texture spriteImage;
 	private Sound bellSound;
-	private Rectangle spriteRect;
-	private Rectangle screenRect = new Rectangle();
-	private float dx = 1, dy = 1;
+	private CameraController2D camera;
 
 	@Override
 	public void create() {
@@ -28,22 +26,18 @@ public class Game implements ApplicationListener {
 		font = new BitmapFont();
 		font.setColor(Color.RED);
 		spriteImage = new Texture(Gdx.files.internal("obligator.png"));
-		spriteRect = new Rectangle(1, 1, spriteImage.getWidth() / 2f, spriteImage.getHeight() / 2f);
 		bellSound = Gdx.audio.newSound(Gdx.files.internal("blipp.ogg"));
 		Gdx.graphics.setForegroundFPS(60);
+
+		camera = new CameraController2D(16, 16);
+		camera.screenAnchor = new Vector2(0,0);
 	}
 
 	@Override
 	public void dispose() {
 		// Called at shutdown
+		// Unload assets
 
-		// Graphics and sound resources aren't managed by Java's garbage collector, so
-		// they must generally be disposed of manually when no longer needed. But,
-		// any remaining resources are typically cleaned up automatically when the
-		// application exits, so these aren't strictly necessary here.
-		// (We might need to do something like this when loading a new game level in
-		// a large game, for instance, or if the user switches to another application
-		// temporarily (e.g., incoming phone call on a phone, or something).
 		batch.dispose();
 		font.dispose();
 		spriteImage.dispose();
@@ -55,18 +49,6 @@ public class Game implements ApplicationListener {
 	 * @param deltaSeconds Time passed since last frame.
 	 */
 	public void update(float deltaSeconds) {
-		Rectangle.tmp.set(spriteRect);
-		Rectangle.tmp.x += dx;
-		Rectangle.tmp2.set(spriteRect);
-		Rectangle.tmp2.y += dy;
-		if (screenRect.contains(Rectangle.tmp))
-			spriteRect.x += dx;
-		else
-			dx = -dx;
-		if (screenRect.contains(Rectangle.tmp2))
-			spriteRect.y += dy;
-		else
-			dy = -dy;
 
 		// Don't handle input this way – use event handlers!
 		if (Gdx.input.justTouched()) { // check for mouse click
@@ -82,15 +64,15 @@ public class Game implements ApplicationListener {
 	 * Called once per frame after update.
 	 */
 	public void draw() {
+		camera.setTarget(new Vector2(0,0));
+		camera.beginWorld(batch);
 
-		ScreenUtils.clear(Color.WHITE);
+		ScreenUtils.clear(Color.BLACK);
 
 		batch.begin();
 		{ // Draw images here
 			font.draw(batch, "Hello, World!", 200, 200);
-			font.draw(batch, "spriteRect = " + spriteRect.toString(), 10, 20);
-			font.draw(batch, "screenRect = " + screenRect.toString(), 10, 40);
-			batch.draw(spriteImage, spriteRect.x, spriteRect.y, spriteRect.width, spriteRect.height);
+			batch.draw(spriteImage, 0, 0, 1, 1);
 		}
 		batch.end();
 	}
@@ -107,11 +89,7 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void resize(int width, int height) {
-		// Called whenever the window is resized (including with its original site at
-		// startup)
-
-		screenRect.width = width;
-		screenRect.height = height;
+		camera.resize(width, height);
 	}
 
 	@Override
