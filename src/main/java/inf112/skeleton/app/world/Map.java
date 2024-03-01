324f551dac1979d21361a0819d891b835c7456fe
Ivar Fatland <ivar.fatland@lyse.net>
2024-03-01 10:34:54 +0100

    Added skeleton files for world entities

---
+++ src/main/java/inf112/skeleton/app/world/Map.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app.world;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import jdk.jshell.spi.ExecutionControl;
+
+public class Map {
+
+    Block getBlock(int x, int y) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not Implemented");
+    }
+
+    void draw(SpriteBatch sb) {
+
+    }
+}
---
+++ src/main/java/inf112/skeleton/app/world/Player.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app.world;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Player {
+
+    void update(float deltaSeconds) {
+
+    }
+
+    void draw(SpriteBatch sb) {
+
+    }
+
+}
