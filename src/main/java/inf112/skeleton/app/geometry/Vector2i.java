ae94ef9dfea87810cb3d7b73b95f150ebc54c936
Ivar Fatland <ivar.fatland@lyse.net>
2024-03-08 20:01:31 +0100

    Added equals method to Vector2i.

--- src/main/java/inf112/skeleton/app/geometry/Vector2i.java
+++ src/main/java/inf112/skeleton/app/geometry/Vector2i.java
@@ -5,13 +5,20 @@ public record Vector2i(int x, int y) {
     public Vector2i add(Vector2i other) {
         return new Vector2i(x + other.x, y + other.y);
     }
+
     public Vector2i sub(Vector2i other) {
         return new Vector2i(x - other.x, y - other.y);
     }
+
     public Vector2i mul(Vector2i other) {
         return new Vector2i(x * other.x, y * other.y);
     }
+
     public Vector2i div(Vector2i other) {
         return new Vector2i(x / other.x, y / other.y);
+    }
+
+    public boolean equals(Vector2i other) {
+        return (this.x == other.x) && (this.y == other.y);
     }
 }
