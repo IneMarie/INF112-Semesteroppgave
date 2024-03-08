32d1452e370e50e9aa9b7e1cc3868071141f3878
Ivar Fatland <ivar.fatland@lyse.net>
2024-03-08 20:00:21 +0100

    changed how entity moves, added update and draw

--- src/main/java/inf112/skeleton/app/world/IWorld.java
+++ src/main/java/inf112/skeleton/app/world/IWorld.java
@@ -1,12 +1,18 @@
 package inf112.skeleton.app.world;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.geometry.Vector2i;
 
 public interface IWorld {
 
     /**
      * The world is responsible for determining whether the Entity is allowed to move.
-     * @return New position
+     *
+     * @return true if entity moved.
      */
-    Vector2i moveEntity(IEntity entity, Vector2i movement);
+    boolean moveEntity(IEntity entity, Vector2i movement);
+
+    void update(float deltaSeconds);
+
+    void draw(SpriteBatch batch);
 }
