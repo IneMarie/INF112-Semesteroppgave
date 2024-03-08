1e8972480914d89b53751b4ab83bf1090a2bdfe9
Ivar Fatland <ivar.fatland@lyse.net>
2024-03-08 19:59:27 +0100

    Expanded Entity interface with update, draw and setPosition.

--- src/main/java/inf112/skeleton/app/world/IEntity.java
+++ src/main/java/inf112/skeleton/app/world/IEntity.java
@@ -1,12 +1,18 @@
 package inf112.skeleton.app.world;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.geometry.Vector2i;
-import inf112.skeleton.app.world.Tile;
 
 public interface IEntity {
     Vector2i getPosition();
 
+    void setPosition(Vector2i new_position);
+
     Tile getTile();
+
+    void update(float deltaSeconds);
+
+    void draw(SpriteBatch batch);
 }
 
 
