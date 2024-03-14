1278d35d6dbff3e673414872a9126c0955e43959
Ine Marie <inemarie.t@gmail.com>
2024-03-14 12:45:24 +0100

    Lagt inn (midlertidig) tekst i gameover og title sceen

--- src/main/java/inf112/skeleton/app/screens/GameOverScreen.java
+++ src/main/java/inf112/skeleton/app/screens/GameOverScreen.java
@@ -2,23 +2,21 @@ package inf112.skeleton.app.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import inf112.skeleton.app.GameLogic;
-
 
 public class GameOverScreen implements Screen{
   private final ScreenManager screenManager; 
-  private final SpriteBatch batch;  
+  private final BitmapFont font;
+  private final SpriteBatch batch; 
   
   public GameOverScreen(ScreenManager screenManager){
     this.screenManager = screenManager;
+    font = new BitmapFont();
     batch = new SpriteBatch();
   }
   
@@ -31,10 +29,13 @@ public class GameOverScreen implements Screen{
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    // Sjekker om Enter
-    if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-      System.out.println("Current screen: GameOverScreen");
-    }
+    // Tegner tekst
+    batch.begin();
+    font.setColor(Color.WHITE);
+    float x = Gdx.graphics.getWidth() / 2f - 100;
+    float y = Gdx.graphics.getHeight() / 2f;
+    font.draw(batch, "Denne teksten e cursed", x, y); // Teksten e for liten uansett ka eg gjør TODO
+    batch.end();
   }
   
   @Override
@@ -55,6 +56,7 @@ public class GameOverScreen implements Screen{
   
   @Override
   public void dispose() {
+    font.dispose();
     batch.dispose();
   }
 }
--- src/main/java/inf112/skeleton/app/screens/TitleScreen.java
+++ src/main/java/inf112/skeleton/app/screens/TitleScreen.java
@@ -2,18 +2,21 @@ package inf112.skeleton.app.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-
-import inf112.skeleton.app.GameLogic;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TitleScreen implements Screen{
-  private final ScreenManager screenManager; 
+  private final ScreenManager screenManager;
+  private final BitmapFont font;
+  private final SpriteBatch batch; 
   
   public TitleScreen(ScreenManager screenManager){
     this.screenManager = screenManager;
+    font = new BitmapFont();
+    batch = new SpriteBatch();
   }
   
   @Override
@@ -22,7 +25,6 @@ public class TitleScreen implements Screen{
   
   @Override
   public void render(float delta) {
-    // RØd farge
     Gdx.gl.glClearColor(0, 0, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     
@@ -31,6 +33,14 @@ public class TitleScreen implements Screen{
       System.out.println("Byttet til GameScreen");
       screenManager.showGameScreen();
     }
+    
+    // Tegner tekst
+    batch.begin();
+    font.setColor(Color.WHITE);
+    float x = Gdx.graphics.getWidth() / 2f - 100;
+    float y = Gdx.graphics.getHeight() / 2f;
+    font.draw(batch, "Press Enter to play", x, y);
+    batch.end();
   }
   
   @Override
@@ -51,5 +61,7 @@ public class TitleScreen implements Screen{
   
   @Override
   public void dispose() {
+    font.dispose();
+    batch.dispose();
   }
 }
