152a997a8264e45d83b7748445220cb52a9fc194
Ine Marie <inemarie.t@gmail.com>
2024-03-18 09:49:15 +0100

    Nye lava textures

--- src/main/java/inf112/skeleton/app/assets/Textures.java
+++ src/main/java/inf112/skeleton/app/assets/Textures.java
@@ -7,7 +7,8 @@ public enum Textures {
     Player("Dungeon Crawl Stone Soup Full/player/base/centaur_darkbrown_male.png"),
     Floor("Dungeon Crawl Stone Soup Full/dungeon/floor/grass/grass0-dirt-mix_1.png"),
     Wall("Dungeon Crawl Stone Soup Full/dungeon/wall/brick_brown_2.png"),
-    Lava("Dungeon Crawl Stone Soup Full/dungeon/floor/lava_2.png"),
+    Lava("lava.png"),
+    LavaTop("lavatop.png"),
     Snake("Dungeon Crawl Stone Soup Full/monster/animals/snake.png"),
     Boulder("rock placeholder.png");
     
